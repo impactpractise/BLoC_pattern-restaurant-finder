b2264625b072645f3cad77d72c1295f8b5f942f8
Andre <andre.bappert@gmail.com>
2020-02-24 08:27:29 +0100

    Initial app setup

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/andre/code/impactpractise/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/andre/AndroidStudioProjects/Restaurant-Finder/restaurant-finder-starter"
+export "FLUTTER_TARGET=/Users/andre/AndroidStudioProjects/Restaurant-Finder/restaurant-finder-starter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/andre/code/impactpractise/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
